2d18b2772feaaf63c7ea087b6442df987f543b66
Ian Watson <ianwatson@ianwatson-OSX.local>
2016-04-27 11:25:05 +0100

    Adding sample data

--- sql/Customer.sql
+++ sql/Customer.sql
@@ -4,3 +4,7 @@ customer_id int,
 customer_name varchar(255),
 customer_address varchar(255)
 );
+
+INSERT INTO customer VALUES (1,'Ian','Northfield');
+INSERT INTO customer VALUES (2,'Dan','Northfield');
+
